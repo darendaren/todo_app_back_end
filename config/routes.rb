79f969fc8b6366d5da4076cffd2c0f6b68d2a42c
Daren B <snoopinwinslet@gmail.com>
2017-10-01 19:11:46 +0000

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-   get 'todo/index', to: 'todo#index'
+   root to: 'todo#index'
    get 'todo/show', to: 'todo#show'
    get 'todo/new', to: 'todo#new'
    get 'todo/show/:id', to: 'todo#show'
    get 'todo/create', to: 'todo#create'
    post 'todo/new', to: 'todo#create'
    get 'todo/edit/:id', to: 'todo#edit'
-   put 'todo/update/:id', to: 'todo#update'
-   put 'todo/edit/:id/destroy', to: 'todo#destroy'
- 
+   get 'todo/update/:id', to: 'todo#update'
+   get 'todo/edit/:id/destroy', to: 'todo#destroy'
   end 
-  
-
