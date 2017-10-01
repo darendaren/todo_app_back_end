79aaf4b00d31347b7bcdf891c80fc95ed42c59ba
Daren B <snoopinwinslet@gmail.com>
2017-10-01 16:31:22 +0000

    stuff

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -16,4 +16,14 @@ class TodoController < ApplicationController
     t.save
     redirect_to "/todo/show/#{ t.id }"
   end 
+  
+  def edit
+    @todo = Todo.find_by_id(params[:id])
+  end
+  
+  def destroy
+  t = Todo.find_by_id(params[:id])
+  t.destroy
+  redirect_to "/todo/index"
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
    get 'todo/show/:id', to: 'todo#show'
    get 'todo/create', to: 'todo#create'
    post 'todo/new', to: 'todo#create'
+   get 'todo/edit/:id', to: 'todo#edit'
+   put 'todo/update/:id', to: 'todo#update'
+   put 'todo/edit/:id/destroy', to 'todo#destroy'
   end 
   
 
