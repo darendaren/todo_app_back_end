0a5ecaaeb0a67f83c12b41adc4be56e6a36f32ee
Daren B <snoopinwinslet@gmail.com>
2017-10-01 14:54:37 +0000

    Remove conditional logic and pull data straight from the datatbase

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,25 +3,6 @@ class TodoController < ApplicationController
   end
   
   def show
-    todo_id= params[:id]
-    todo_id = todo_id.to_i
-    if (todo_id == 1)
-      @description = 'Make the curriculum'
-      @estimate = '4'
-      
-    elsif (todo_id == 2)  
-      @description = 'Buy food'
-      @estimate = 3
-
-    elsif (todo_id ==3)
-      @dsecription = 'call person'
-      @estimate = 2
-      
-    elsif (todo_id == 4)
-      @description = 'clean up'
-      @estimate = 1 
-  
-    end 
-    
+    @todo = Todo.find_by_id(params[:id])
   end 
 end
