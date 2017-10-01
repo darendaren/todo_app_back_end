caa0c7bf127de9bc085131adf6a8447e8c368e11
Daren B <snoopinwinslet@gmail.com>
2017-10-01 16:01:17 +0000

    todoz

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
    get 'todo/index', to: 'todo#index'
    get 'todo/show', to: 'todo#show'
+   get 'todo/new', to: 'todo#new'
    get 'todo/show/:id', to: 'todo#show'
+   get 'todo/create', to: 'todo#create'
+   post 'todo/new', to: 'todo#create'
   end 
   
 
